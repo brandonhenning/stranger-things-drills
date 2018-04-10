c13d2e0588418ea72f9b3ddc55302cbb29858f9c
Brandon Henning <34727711+brandonhenning@users.noreply.github.com>
2018-04-10 10:54:05 -0600

    broke app.js into smaller functions

--- version-1/scripts/app.js
+++ version-1/scripts/app.js
@@ -1,36 +1,49 @@
-const url = ('https://quiet-bayou-99554.herokuapp.com/api/v1/contacts');
-
-function createPage () {
-    fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data.data)
-        return(data.data);
-    })
-    .then((array) => {
-        array.forEach(element => {
-            console.log(element.name)
-            let parent = document.getElementById('characters');
-            let li = document.createElement('li')
-            let img = document.createElement('img')
-            let description = document.createElement('p')
-            let span = document.createElement('span')
-            let a = document.createElement('a')
-            a.textContent = `Leave ${element.name} a message`
-            a.href = (`../contact.html?character=${element.name}`)
-            span.textContent = `${element.name} - ${element.phone}`
-            description.textContent = element.message;
-            img.src = element.imageURL
-            parent.appendChild(li)
-            li.appendChild(img)
-            li.appendChild(description)
-            li.appendChild(span)
-            li.appendChild(a)
-        });
-    })
+function createPage(){
+    const url = 'https://quiet-bayou-99554.herokuapp.com/api/v1/contacts'
+    fetch (url)
+        .then(response => response.json())
+        .then(response => response.data)
+        .then(data => data.forEach(buildList))
 }
 
-createPage();
+function buildList(character){
+    const li = document.createElement('li')
+    const elements = [
+        createImage(character.imageURL, character.name),
+        createName(character.name, character.phone),
+        createMessage(character.message),
+        addLink(character.name)
+    ].forEach(element => li.appendChild(element))
 
+    document.querySelector('#characters').appendChild(li)
+}
+
+function createImage(imageUrl, altText){
+    const image = document.createElement('img')
+    image.src = imageUrl
+    image.alt = altText
+    return image
+}
+
+function createName(name, phoneNumber){
+    return buildElement('span', `${name} - ${phoneNumber}`)
+}
+
+function createMessage(message){
+    return buildElement('p', message)
+}
+
+function addLink(name){
+    const a = buildElement('a', `Leave ${name} a message`)
+    a.href = `contact.html?character=${name}`
+    return a
+}
+
+function buildElement(tagName, text){
+    const element = document.createElement(tagName)
+    const msg = document.createTextNode(text)
+    element.appendChild(msg)
+    return element
+}
+
+createPage()
